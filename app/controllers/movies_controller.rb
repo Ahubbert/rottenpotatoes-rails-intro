9afeba17bc8c2914c9d896ba75a6a135c4d08fe8
ahubbert <ahubbert@uncc.edu>
2015-11-05 03:38:46 +0000

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,32 +2,8 @@
 class MoviesController < ApplicationController
 
   def index
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
-
-    session[:ratings] = params[:ratings] if params[:ratings]
-    session[:sort]    = params[:sort]    if params[:sort]
-
-    if session[:ratings] || session[:sort]
-      case session[:sort]
-      when 'title'
-        @title_hilite = 'hilite'
-      when 'release_date'
-        @release_hilite = 'hilite'
-      end
-
-      session[:ratings] ||= @all_ratings
-      @ratings = session[:ratings]
-      @ratings = @ratings.keys if @ratings.respond_to?(:keys)
-      @movies = Movie.find(:all,
-                           order: session[:sort],
-                           conditions: ["rating IN (?)", @ratings])
-    else
-      @movies = Movie.all
-    end
-
-    if session[:ratings] != params[:ratings] || session[:sort] != params[:sort]
-      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort])
-    end
+    @movies = Movie.find(:all, :order => (params[:sort_by]))
+    @sort_column = params[:sort_by]
   end
 
   def show
